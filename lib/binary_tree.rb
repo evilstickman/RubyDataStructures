7e90caf77a311c619318135a30c8b1ff4f52bc18
Matt Billock <mattbillock@mattbillock.com>
2016-09-25 12:05:05 -0700

    Added traversals - recursive depth-first, and iterative breadth-first

--- lib/binary_tree.rb
+++ lib/binary_tree.rb
@@ -56,5 +56,37 @@ module Datatypes
       srand
       rand(2) == 1 ? BinaryTree::LEFT : BinaryTree::RIGHT
     end
+
+    def pre_order_traversal_recursive(node)
+      puts node.data if node
+      pre_order_traversal_recursive(node.left) unless node.nil?
+      pre_order_traversal_recursive(node.right) unless node.nil?
+    end
+    def in_order_traversal_recursive(node)
+      in_order_traversal_recursive(node.left) unless node.nil?
+      puts node.data if node
+      in_order_traversal_recursive(node.right) unless node.nil?
+    end
+    def post_order_traversal_recursive(node)
+      post_order_traversal_recursive(node.left) unless node.nil?
+      post_order_traversal_recursive(node.right) unless node.nil?
+      puts node.data if node
+    end
+    def level_order_traversal(node)
+      queue = Queue.new
+      if node
+        puts node.data
+        queue << node.left
+        queue << node.right
+      end
+      while(queue.size > 0)
+        new_node = queue.pop
+        if new_node
+          puts new_node.data
+          queue << new_node.left
+          queue << new_node.right
+        end
+      end
+    end
   end
 end
